2925ee07abbb070057379d882b8a5374bde458bd
mnarayan <mnarayan@users.noreply.github.com>
2018-06-09 20:19:01 -0700

    Fixed errors when df contains object type

--- combat.py
+++ combat.py
@@ -10,6 +10,7 @@ def adjust_nums(numerical_covariates, drop_idxs):
     if numerical_covariates is None: return drop_idxs
     return [nc - sum(nc < di for di in drop_idxs) for nc in numerical_covariates]
 
+
 def design_mat(mod, numerical_covariates, batch_levels):
     # require levels to make sure they are in the same order as we use in the
     # rest of the script.
@@ -37,7 +38,6 @@ def design_mat(mod, numerical_covariates, batch_levels):
 
 def combat(data, batch, model=None, numerical_covariates=None):
     """Correct for batch effects in a dataset
-
     Parameters
     ----------
     data : pandas.DataFrame
@@ -53,7 +53,6 @@ def combat(data, batch, model=None, numerical_covariates=None):
     numerical_covariates : list-like
         List of covariates in the model which are numerical, rather than
         categorical
-
     Returns
     -------
     corrected : pandas.DataFrame
@@ -88,13 +87,14 @@ def combat(data, batch, model=None, numerical_covariates=None):
     sys.stderr.write("Standardizing Data across genes.\n")
     B_hat = np.dot(np.dot(la.inv(np.dot(design.T, design)), design.T), data.T)
     grand_mean = np.dot((n_batches / n_array).T, B_hat[:n_batch,:])
-    var_pooled = np.dot(((data - np.dot(design, B_hat).T)**2), np.ones((int(n_array), 1)) / int(n_array))
+    var_pooled = np.dot(np.array(((data - np.dot(design, B_hat).T)**2),dtype=np.float32), np.ones((int(n_array), 1)) / int(n_array))
 
     stand_mean = np.dot(grand_mean.T.reshape((len(grand_mean), 1)), np.ones((1, int(n_array))))
     tmp = np.array(design.copy())
     tmp[:,:n_batch] = 0
     stand_mean  += np.dot(tmp, B_hat).T
 
+    var_pooled = np.array(var_pooled,dtype=np.float32)
     s_data = ((data - stand_mean) / np.dot(np.sqrt(var_pooled), np.ones((1, int(n_array)))))
 
     sys.stderr.write("Fitting L/S model and finding priors\n")
@@ -109,7 +109,7 @@ def combat(data, batch, model=None, numerical_covariates=None):
 
     gamma_bar = gamma_hat.mean(axis=1) 
     t2 = gamma_hat.var(axis=1)
-   
+
 
     a_prior = list(map(aprior, delta_hat))
     b_prior = list(map(bprior, delta_hat))
@@ -140,14 +140,15 @@ def combat(data, batch, model=None, numerical_covariates=None):
         numer = np.array(bayesdata[batch_idxs] - np.dot(batch_design.loc[batch_idxs], gamma_star).T)
 
         bayesdata[batch_idxs] = numer / denom
-   
+
     vpsq = np.sqrt(var_pooled).reshape((len(var_pooled), 1))
     bayesdata = bayesdata * np.dot(vpsq, np.ones((1, int(n_array)))) + stand_mean
- 
-    return bayesdata
+
+    return bayesdata,gamma_star,delta_star
+
 
 def it_sol(sdat, g_hat, d_hat, g_bar, t2, a, b, conv=0.0001):
-    n = (1 - np.isnan(sdat)).sum(axis=1)
+    n = (1 - pd.isnull(sdat)).sum(axis=1)
     g_old = g_hat.copy()
     d_old = d_hat.copy()
 
@@ -158,7 +159,7 @@ def it_sol(sdat, g_hat, d_hat, g_bar, t2, a, b, conv=0.0001):
         g_new = postmean(g_hat, g_bar, n, d_old, t2)
         sum2 = ((sdat - np.dot(g_new.values.reshape((g_new.shape[0], 1)), np.ones((1, sdat.shape[1])))) ** 2).sum(axis=1)
         d_new = postvar(sum2, n, a, b)
-       
+
         change = max((abs(g_new - g_old) / g_old).max(), (abs(d_new - d_old) / d_old).max())
         g_old = g_new #.copy()
         d_old = d_new #.copy()
@@ -166,20 +167,22 @@ def it_sol(sdat, g_hat, d_hat, g_bar, t2, a, b, conv=0.0001):
     adjust = (g_new, d_new)
     return adjust 
 
-    
 
 def aprior(gamma_hat):
     m = gamma_hat.mean()
     s2 = gamma_hat.var()
     return (2 * s2 +m**2) / s2
 
+
 def bprior(gamma_hat):
     m = gamma_hat.mean()
     s2 = gamma_hat.var()
     return (m*s2+m**3)/s2
 
+
 def postmean(g_hat, g_bar, n, d_star, t2):
     return (t2*n*g_hat+d_star * g_bar) / (t2*n+d_star)
+
 
 def postvar(sum2, n, a, b):
     return (0.5 * sum2 + b) / (n / 2.0 + a - 1.0)
